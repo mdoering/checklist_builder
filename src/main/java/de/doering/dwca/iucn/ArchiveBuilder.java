d014dd2476373d913677bef0d55f7e1d83ae2050
Matthew Blissett <matt@blissett.me.uk>
2020-11-03 15:50:44 +0100

    Changes to IUCN archive after meeting: License, split download, minor fixes.
    Resolves https://github.com/gbif/data-mobilization/issues/175

--- src/main/java/de/doering/dwca/iucn/ArchiveBuilder.java
+++ src/main/java/de/doering/dwca/iucn/ArchiveBuilder.java
@@ -25,6 +25,7 @@ import org.gbif.api.model.registry.Contact;
 import org.gbif.api.vocabulary.ContactType;
 import org.gbif.api.vocabulary.DatasetType;
 import org.gbif.api.vocabulary.Language;
+import org.gbif.api.vocabulary.License;
 import org.gbif.api.vocabulary.TaxonomicStatus;
 import org.gbif.common.parsers.LanguageParser;
 import org.gbif.common.parsers.core.ParseResult;
@@ -46,13 +47,24 @@ import java.net.URI;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
+/**
+ * An archive builder for the IUCN Red List checklist.
+ *
+ * This converts archives retrieved using bulk downloads from the IUCN website to Darwin Core archives.
+ *
+ * The resulting archive should be publicly accessible.
+ *
+ * An earlier version (cmmit 97f9555cb58a9a57b88c5b57d8b0d71f895bceb4) used the IUCN API, but for
+ * performance was replaced with the downloads.  The API could be used once more, if synonyms,
+ * author names and citations are added to the "bulk list" endpoint.
+ */
 public class ArchiveBuilder extends AbstractBuilder {
 
   // metadata
@@ -63,16 +75,23 @@ public class ArchiveBuilder extends AbstractBuilder {
   private static final String CITATION_FORMAT = "IUCN ({YEAR}). The IUCN Red List of Threatened Species. " +
     "Version {VERSION}. https://www.iucnredlist.org. Downloaded on {DL_DATE}. https://doi.org/10.15468/0qnb58";
 
-  // This is a download from the IUCN Red List site, created using a search from https://www.iucnredlist.org/search
+  public static final Pattern DX_DOI = Pattern.compile("https?://dx\\.doi\\.org/");
+
+  // These are two downloads from the IUCN Red List site, created using searches from https://www.iucnredlist.org/search
   //
-  // Unfortunately, it seems not to be possible to link to the search.
+  // The two searches have
   // • Geographical Scope: Global
   // • Include: Species; Subspecies and varieties
-  // Then it is downloaded in "Search Summary" format, as the alternative download formats are restricted.
+  // • Taxonomy:
+  //   • Everything except Passeriformes: https://www.iucnredlist.org/search?dl=true&permalink=00b3c1db-8c63-46a2-9ca6-b517bcf577f2
+  //   • Passeriformes: https://www.iucnredlist.org/search?dl=true&permalink=c7a8b5ed-c6cf-4151-836a-39b3ecb31784
+  // Then it is downloaded in "Search Results" format.  Splitting the birds in two is necessary
+  // to enable use of the "Search Results" format.
   //
-  // Commit 97f9555cb58a9a57b88c5b57d8b0d71f895bceb4 used the IUCN API, which included the DOIs for
-  // each assessment.  These are unfortunately not included in the downloads.
-  private static final String DOWNLOAD = "https://hosted-datasets.gbif.org/datasets/iucn/redlist_species_data_565c8122-6fa3-497d-b873-9aa807a7bf05.zip";
+  // The downloads are stored in a private location in accordance with the IUCN terms and conditions.
+  private static final String DOWNLOAD_1 = "https://hosted-datasets.gbif.org/datasets/protected/iucn/redlist_species_data_002cb0d2-af95-4875-9014-668ac1266396.zip";
+  private static final String DOWNLOAD_2 = "https://hosted-datasets.gbif.org/datasets/protected/iucn/redlist_species_data_db89a551-13d9-4ed9-910d-9e5f10b1f228.zip"; // Passeriformes
+
   private static final String VERSION_URL = "https://apiv3.iucnredlist.org/api/v3/version";
 
   // columns for taxonomy.csv
@@ -126,6 +145,12 @@ public class ArchiveBuilder extends AbstractBuilder {
   private static final int COM_LANGUAGE = 3;
   private static final int COM_MAIN = 4;
 
+  // columns for dois.csv
+  private static final int DOI_ASSESSMENT_ID = 0;
+  private static final int DOI_SCIENTIFIC_NAME = 1;
+  private static final int DOI_INTERNAL_TAXON_ID = 2;
+  private static final int DOI_DOI = 3;
+
   // columns for synonyms.csv
   private static final int SYN_INTERNAL_TAXON_ID = 0;
   private static final int SYN_SCIENTIFIC_NAME = 1;
@@ -151,132 +176,158 @@ public class ArchiveBuilder extends AbstractBuilder {
     // Publication date of this checklist
     dataset.setPubDate(new Date());
 
-    final File tmp = FileUtils.createTempDir();
-    tmp.deleteOnExit();
+    int count = 0;
+    for (String downloadFile : new String[]{DOWNLOAD_1, DOWNLOAD_2}) {
+      final File tmp = FileUtils.createTempDir();
+      tmp.deleteOnExit();
 
-    final File zip = File.createTempFile("iucn", ".zip");
-    zip.deleteOnExit();
-    http.download(DOWNLOAD, zip);
-    List<File> files = CompressionUtil.unzipFile(tmp, zip);
+      final File zip = File.createTempFile("iucn", ".zip");
+      zip.deleteOnExit();
+      http.download(downloadFile, zip);
+      List<File> files = CompressionUtil.unzipFile(tmp, zip);
 
-    // The simple_summary.csv file was probably sufficient, but isn't used.
-    // Optional<File> simple_summary = files.stream().filter(f -> f.getName().equals("simple_summary.csv")).findFirst();
+      // The simple_summary.csv file was probably sufficient, but isn't used.
+      // Optional<File> simple_summary = files.stream().filter(f -> f.getName().equals("simple_summary.csv")).findFirst();
 
-    // Index assessments by taxon key
-    Multimap<String, List<String>> assessmentsMap =
-      indexByColumn(files.stream().filter(f -> f.getName().equals("assessments.csv")).findFirst().get(), ASS_INTERNAL_TAXON_ID);
+      // Index assessments by taxon key
+      Multimap<String, List<String>> assessmentsMap =
+        indexByColumn(files.stream().filter(f -> f.getName().equals("assessments.csv")).findFirst().get(), ASS_INTERNAL_TAXON_ID);
 
-    // Index common names by taxon key
-    Multimap<String, List<String>> commonNamesMap =
-      indexByColumn(files.stream().filter(f -> f.getName().equals("common_names.csv")).findFirst().get(), COM_INTERNAL_TAXON_ID);
+      // Index common names by taxon key
+      Multimap<String, List<String>> commonNamesMap =
+        indexByColumn(files.stream().filter(f -> f.getName().equals("common_names.csv")).findFirst().get(), COM_INTERNAL_TAXON_ID);
 
-    // Index synonyms by taxon key
-    Multimap<String, List<String>> synonymsMap =
-      indexByColumn(files.stream().filter(f -> f.getName().equals("synonyms.csv")).findFirst().get(), SYN_INTERNAL_TAXON_ID);
+      // Index DOIs by taxon key
+      Multimap<String, List<String>> doisMap =
+        indexByColumn(files.stream().filter(f -> f.getName().equals("dois.csv")).findFirst().get(), DOI_INTERNAL_TAXON_ID);
 
-    // Iterate through the taxonomy
-    Optional<File> taxonomy = files.stream().filter(f -> f.getName().equals("taxonomy.csv")).findFirst();
-    TabularDataFileReader<List<String>> reader = TabularFiles.newTabularFileReader(
-      new InputStreamReader(new FileInputStream(taxonomy.get()), "UTF-8"),
-      ',', "\n", '"', true
-    );
+      // Index synonyms by taxon key
+      Multimap<String, List<String>> synonymsMap =
+        indexByColumn(files.stream().filter(f -> f.getName().equals("synonyms.csv")).findFirst().get(), SYN_INTERNAL_TAXON_ID);
 
-    List<String> taxon;
-    while ((taxon = reader.read()) != null) {
-      final String taxonKey = taxon.get(TAX_TAXON_ID);
+      // Iterate through the taxonomy
+      Optional<File> taxonomy = files.stream().filter(f -> f.getName().equals("taxonomy.csv")).findFirst();
+      TabularDataFileReader<List<String>> reader = TabularFiles.newTabularFileReader(
+        new InputStreamReader(new FileInputStream(taxonomy.get()), "UTF-8"),
+        ',', "\n", '"', true
+      );
 
-      // Skip subpopulation assessments, in case these have been included in the download
-      if (!Strings.isNullOrEmpty(taxon.get(TAX_SUBPOPULATION_NAME))) {
-        LOG.info("Skipping {}, which is a subpopulation taxon", taxonKey);
-        continue;
-      }
+      List<String> taxon;
+      while ((taxon = reader.read()) != null) {
+        final String taxonKey = taxon.get(TAX_TAXON_ID);
 
-      final String authority = taxon.get(TAX_AUTHORITY).replace("&amp;", "&");
-
-      writer.newRecord(taxonKey);
-      writer.addCoreColumn(DwcTerm.scientificName, taxon.get(TAX_SCIENTIFIC_NAME) + ' ' + authority);
-      writer.addCoreColumn(DwcTerm.kingdom, taxon.get(TAX_KINGDOM_NAME));
-      writer.addCoreColumn(DwcTerm.phylum, taxon.get(TAX_PHYLUM_NAME));
-      writer.addCoreColumn(DwcTerm.class_, taxon.get(TAX_CLASS_NAME));
-      writer.addCoreColumn(DwcTerm.order, taxon.get(TAX_ORDER_NAME));
-      writer.addCoreColumn(DwcTerm.family, taxon.get(TAX_FAMILY_NAME));
-      writer.addCoreColumn(DwcTerm.genus, taxon.get(TAX_GENUS_NAME));
-      writer.addCoreColumn(DwcTerm.specificEpithet, taxon.get(TAX_SPECIES_NAME));
-      writer.addCoreColumn(DwcTerm.scientificNameAuthorship, authority);
-      writer.addCoreColumn(DwcTerm.taxonRank, taxon.get(TAX_INFRA_TYPE));
-      writer.addCoreColumn(DwcTerm.infraspecificEpithet, TAX_INFRA_NAME);
-      writer.addCoreColumn(DwcTerm.taxonomicStatus, TaxonomicStatus.ACCEPTED);
-      writer.addCoreColumn(DwcTerm.acceptedNameUsageID, taxonKey);
-      writer.addCoreColumn(DcTerm.references, "https://apiv3.iucnredlist.org/api/v3/taxonredirect/" + taxonKey);
-
-      for (List<String> assessment : assessmentsMap.get(taxonKey)) {
-        Map<Term, String> globalDistribution = new HashMap<>();
-        globalDistribution.put(DwcTerm.locality, "Global");
-        globalDistribution.put(IucnTerm.threatStatus, assessment.get(ASS_REDLIST_CATEGORY));
-        // What about criteria? Population trend?
-        switch (assessment.get(ASS_REDLIST_CATEGORY)) {
-          case "Extinct":
-          case "Extinct in the Wild":
-            globalDistribution.put(DwcTerm.occurrenceStatus, "Absent");
-            break;
-
-          case "Critically Endangered":
-          case "Endangered":
-          case "Vulnerable":
-          case "Near Threatened":
-          case "Data Deficient":
-          case "Least Concern":
-          case "Lower Risk/conservation dependent":
-          case "Lower Risk/near threatened":
-          case "Lower Risk/least concern":
-            globalDistribution.put(DwcTerm.occurrenceStatus, "Present");
-            break;
-
-          case "Not Evaluated": // Not used.
-            globalDistribution.put(DwcTerm.occurrenceStatus, "Unknown");
-            break;
-
-          default:
-            throw new Exception("Unknown assessment category " + assessment.get(ASS_REDLIST_CATEGORY) + " on " + taxonKey);
+        // Skip subpopulation assessments, in case these have been included in the download
+        if (!Strings.isNullOrEmpty(taxon.get(TAX_SUBPOPULATION_NAME))) {
+          LOG.info("Skipping {}, which is a subpopulation taxon", taxonKey);
+          continue;
         }
-        globalDistribution.put(DwcTerm.countryCode, null);
-        globalDistribution.put(DwcTerm.establishmentMeans, null);
-        writer.addExtensionRecord(GbifTerm.Distribution, globalDistribution);
-      }
 
-      for (List<String> commonName : commonNamesMap.get(taxonKey)) {
-        Map<Term, String> vernacularName = new HashMap<>();
-
-        String language = commonName.get(COM_LANGUAGE);
-        ParseResult<Language> parsedLanguage = LanguageParser.getInstance().parse(language);
-        if (parsedLanguage.isSuccessful()) {
-          vernacularName.put(DcTerm.language, parsedLanguage.getPayload().getIso3LetterCode());
-        } else {
-          vernacularName.put(DcTerm.language, language);
+        String citation = null;
+        for (List<String> doi : doisMap.get(taxonKey)) {
+          citation = DX_DOI.matcher(doi.get(DOI_DOI)).replaceAll("https://doi.org/");
         }
-        vernacularName.put(DwcTerm.vernacularName, commonName.get(COM_NAME));
-        vernacularName.put(GbifTerm.isPreferredName, commonName.get(COM_MAIN));
-        writer.addExtensionRecord(GbifTerm.VernacularName, vernacularName);
-      }
+        assert (citation != null);
 
-      // Synonyms make new records, so they must be last.
-      int synonym_index = 0;
-      for (List<String> synonym : synonymsMap.get(taxonKey)) {
-        synonym_index++;
+        final String authority = taxon.get(TAX_AUTHORITY).replace("&amp;", "&").trim();
+        assert (authority != null);
 
-        String synonymName = synonym.get(SYN_NAME).replace("&amp;", "&");
-        String synonymAuthority = synonym.get(SYN_SPECIES_AUTHOR).replace("&amp;", "&");
+        // Calculate rank
+        String rank = "species";
+        if (!Strings.isNullOrEmpty(taxon.get(TAX_INFRA_TYPE))) {
+          rank = taxon.get(TAX_INFRA_TYPE);
+        }
 
-        writer.newRecord(String.valueOf(taxonKey) + "_" + synonym_index);
-        writer.addCoreColumn(DwcTerm.scientificName, synonymName);
-        writer.addCoreColumn(DwcTerm.kingdom, taxon.get(TAX_KINGDOM_NAME)); // Assume synonym is same kingdom as accepted name
-        writer.addCoreColumn(DwcTerm.scientificNameAuthorship, synonymAuthority);
-        writer.addCoreColumn(DwcTerm.taxonomicStatus, TaxonomicStatus.SYNONYM);
+        writer.newRecord(taxonKey);
+        writer.addCoreColumn(DwcTerm.scientificName, taxon.get(TAX_SCIENTIFIC_NAME) + ' ' + authority);
+        writer.addCoreColumn(DwcTerm.kingdom, taxon.get(TAX_KINGDOM_NAME));
+        writer.addCoreColumn(DwcTerm.phylum, taxon.get(TAX_PHYLUM_NAME));
+        writer.addCoreColumn(DwcTerm.class_, taxon.get(TAX_CLASS_NAME));
+        writer.addCoreColumn(DwcTerm.order, taxon.get(TAX_ORDER_NAME));
+        writer.addCoreColumn(DwcTerm.family, taxon.get(TAX_FAMILY_NAME));
+        writer.addCoreColumn(DwcTerm.genus, taxon.get(TAX_GENUS_NAME));
+        writer.addCoreColumn(DwcTerm.specificEpithet, taxon.get(TAX_SPECIES_NAME));
+        writer.addCoreColumn(DwcTerm.scientificNameAuthorship, authority);
+        writer.addCoreColumn(DwcTerm.taxonRank, rank);
+        writer.addCoreColumn(DwcTerm.infraspecificEpithet, taxon.get(TAX_INFRA_NAME));
+        writer.addCoreColumn(DwcTerm.taxonomicStatus, TaxonomicStatus.ACCEPTED);
         writer.addCoreColumn(DwcTerm.acceptedNameUsageID, taxonKey);
+        writer.addCoreColumn(DcTerm.bibliographicCitation, citation);
+        writer.addCoreColumn(DcTerm.references, "https://apiv3.iucnredlist.org/api/v3/taxonredirect/" + taxonKey);
+
+        for (List<String> assessment : assessmentsMap.get(taxonKey)) {
+          Map<Term, String> globalDistribution = new HashMap<>();
+          globalDistribution.put(DwcTerm.locality, "Global");
+          globalDistribution.put(IucnTerm.threatStatus, assessment.get(ASS_REDLIST_CATEGORY));
+          // What about criteria? Population trend?
+          switch (assessment.get(ASS_REDLIST_CATEGORY)) {
+            case "Extinct":
+            case "Extinct in the Wild":
+              globalDistribution.put(DwcTerm.occurrenceStatus, "Absent");
+              break;
+
+            case "Critically Endangered":
+            case "Endangered":
+            case "Vulnerable":
+            case "Near Threatened":
+            case "Data Deficient":
+            case "Least Concern":
+            case "Lower Risk/conservation dependent":
+            case "Lower Risk/near threatened":
+            case "Lower Risk/least concern":
+              globalDistribution.put(DwcTerm.occurrenceStatus, "Present");
+              break;
+
+            case "Not Evaluated": // Not used.
+              globalDistribution.put(DwcTerm.occurrenceStatus, "Unknown");
+              break;
+
+            default:
+              throw new Exception("Unknown assessment category " + assessment.get(ASS_REDLIST_CATEGORY) + " on " + taxonKey);
+          }
+          globalDistribution.put(DwcTerm.countryCode, null);
+          globalDistribution.put(DwcTerm.establishmentMeans, null);
+          globalDistribution.put(DcTerm.source, citation);
+          writer.addExtensionRecord(GbifTerm.Distribution, globalDistribution);
+        }
+
+        for (List<String> commonName : commonNamesMap.get(taxonKey)) {
+          Map<Term, String> vernacularName = new HashMap<>();
+
+          String language = commonName.get(COM_LANGUAGE);
+          ParseResult<Language> parsedLanguage = LanguageParser.getInstance().parse(language);
+          if (parsedLanguage.isSuccessful()) {
+            vernacularName.put(DcTerm.language, parsedLanguage.getPayload().getIso3LetterCode());
+          } else {
+            vernacularName.put(DcTerm.language, language);
+          }
+          vernacularName.put(DwcTerm.vernacularName, commonName.get(COM_NAME));
+          vernacularName.put(GbifTerm.isPreferredName, commonName.get(COM_MAIN));
+          writer.addExtensionRecord(GbifTerm.VernacularName, vernacularName);
+        }
+
+        // Synonyms make new records, so they must be last.
+        int synonym_index = 0;
+        for (List<String> synonym : synonymsMap.get(taxonKey)) {
+          synonym_index++;
+
+          String synonymName = synonym.get(SYN_NAME).replace("&amp;", "&");
+          String synonymAuthority = synonym.get(SYN_SPECIES_AUTHOR).replace("&amp;", "&");
+
+          writer.newRecord(taxonKey + "_" + synonym_index);
+          writer.addCoreColumn(DwcTerm.scientificName, synonymName);
+          writer.addCoreColumn(DwcTerm.kingdom, taxon.get(TAX_KINGDOM_NAME)); // Assume synonym is same kingdom as accepted name
+          writer.addCoreColumn(DwcTerm.scientificNameAuthorship, synonymAuthority);
+          writer.addCoreColumn(DwcTerm.taxonomicStatus, TaxonomicStatus.SYNONYM);
+          writer.addCoreColumn(DwcTerm.acceptedNameUsageID, taxonKey);
+        }
+
+        LOG.info("  Taxon {} ({}) with {} synonyms completed.", taxonKey, taxon.get(TAX_SCIENTIFIC_NAME), synonym_index);
+
+        count++;
       }
 
-      LOG.info("  Taxon {} ({}) with {} synonyms completed.", taxonKey, taxon.get(TAX_SCIENTIFIC_NAME), synonym_index);
     }
+    LOG.info("Processed {} taxa from the IUCN downloads", count);
   }
 
   public static class IucnVersion {
@@ -317,6 +368,9 @@ public class ArchiveBuilder extends AbstractBuilder {
     dataset.setRights("https://www.iucnredlist.org/terms/terms-of-use");
     dataset.setHomepage(HOMEPAGE);
     dataset.setLogoUrl(LOGO);
+    // Craig: the CC-BY-NC license is appropriate, given the IUCN approves the use of the
+    // data on Wikipedia etc, and this is only names plus threat statuses.
+    dataset.setLicense(License.CC_BY_NC_4_0);
 
     Contact craig = new Contact();
     craig.setType(ContactType.ADMINISTRATIVE_POINT_OF_CONTACT);
